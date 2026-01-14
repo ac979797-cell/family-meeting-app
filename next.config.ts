c013029907e04d66783e572eb407cb28e9c8568c
최준영 <ac9797@naver.com>
2026-01-14 16:10:20 +0900

    stress

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
+// next.config.ts
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
